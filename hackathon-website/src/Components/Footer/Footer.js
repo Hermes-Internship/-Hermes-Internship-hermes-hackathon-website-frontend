3eb44e02a8bab34e1b7b6116ada6577a81445e1f
Catapa <catalin24pap@gmail.com>
2021-07-29 10:49:06 +0300

    fix pull request comments

--- hackathon-website/src/Components/Footer/Footer.js
+++ hackathon-website/src/Components/Footer/Footer.js
@@ -1,13 +1,9 @@
 import './Footer.css';
-
 import React from "react";
-
 function Footer() {
     return (
         <footer>
-
             <h1 id={"contact-heading"} className="col-sm-12"><strong>Contact</strong></h1>
-            
             <div className={"row"}>
                 <div id={"address-info"} className={"col-sm-4"}>
                     <span><strong>Adresă:</strong></span>
@@ -17,15 +13,12 @@ function Footer() {
                     <span>Bogdan Petriceicu Hasdeu street, number 45,</span><br/>
                     <span>Cluj-Napoca</span><br/>
                     <span>Romania</span><br/>
-
                 </div>
-
                 <div id={"email-info"} className={"col-sm-4"}>
                     <span><strong>Email:</strong></span>
                     <br/>
                     <span>hackathon@societatahermes.com</span>
                 </div>
-
                 <div id={"socialmedia-info"} className={"col-sm-4"}>
                     <span><strong>Social Media</strong></span>
                     <br/>
@@ -35,15 +28,12 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            
             <div id={"logo-container"} className={"col-sm-12"}>
                 <span>Un eveniment marca:</span>
                 <br/>
                 <img id={"logo"} src={""} alt={"logo hermes"} />
             </div>
-
         </footer>
     )
 }
-
 export default Footer
