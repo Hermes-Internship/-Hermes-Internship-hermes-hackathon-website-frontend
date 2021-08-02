0bffbadbc775b065943d6c2807c4849eca62bf1e
Dorin <turturica.dorin33@gmail.com>
2021-08-02 16:22:27 +0300

    Add Register

--- hackathon-website/src/App.js
+++ hackathon-website/src/App.js
@@ -1,29 +1,11 @@
 import React, { useState } from "react";
-import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
-import SideDrawer from "./Components/SideDrawer/SideDrawer";
-import Backdrop from "./Components/Backdrop/Backdrop";
-import MainView from "./Components/MainView/MainView";
-import Parteners from "./Components/Parteneri/Partners";
+import Register from "./Components/Register/Register";
 
 const App = () => {
-  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
-
-  const drawerToggleClickHandler = () => {
-    setSideDrawerOpen(!sideDrawerOpen);
-  };
-
-  const backdropClickHandler = () => {
-    setSideDrawerOpen(false);
-  };
-
   return (
     <div style={{ height: "100%" }}>
-      <Navbar drawerClickHandler={drawerToggleClickHandler} />
-      <SideDrawer show={sideDrawerOpen} />
-      {sideDrawerOpen ? <Backdrop click={backdropClickHandler} /> : null}
-      <MainView />
-      <Parteners />
+      <Register />
     </div>
   );
 };
